bb7d70c0f626699eb2029ded64dc9aa12c7c3572
aditi-26337 <aditi.26337@gmail.com>
2026-07-22 00:00:49 +0530

    github-actions enabled in CI/CD pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   // Use a relative base so the site works when served from a subpath (GitHub Pages)
   base: './',
 })
